4ed4f0810b7e1316cc4cfd6eb988287df33c4343
Ostitter-Anondo <89913816+Ostitter-Anondo@users.noreply.github.com>
2025-01-15 22:41:32 +0600

    signup data handling

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://b10a11-ostitter-anondo.web.app",
-      "https://b10a11-ostitter-anondo.firebaseapp.com",
     ],
     credentials: true,
   })
@@ -52,6 +50,12 @@ const client = new MongoClient(uri, {
 const database = client.db("studyZone");
 
 const userCol = database.collection("users");
+const reviewCol = database.collection("reviews");
+const noteCol = database.collection("notes");
+const sessionCol = database.collection("sessions");
+const pendingCol = database.collection("pendings");
+const rejectedCol = database.collection("rejected");
+const bookedCol = database.collection("booked");
 
 // general landing
 
@@ -63,4 +67,69 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`we having stuffs happun at ${port}`);
+});
+
+// jwt stuff
+
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: "5h" });
+  const userData = await userCol.findOne(user);
+  const booked = await bookedCol.findOne(user);
+  res
+    .cookie(`studyzonetoken`, token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ userData, booked });
+});
+
+app.post("/logout", (req, res) => {
+  res
+    .clearCookie(`studyzonetoken`, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ success: true });
+});
+
+app.get("/jwtverify", verifyToken, async (req, res) => {
+  if (req.user.uid != req.query.uid) {
+    return res.status(403).send({ message: "forbidden" });
+  }
+
+  res.send("verification success");
+});
+
+// mongo stuff //
+
+// auth stuff
+
+app.get("/login", async(req, res) => {
+  console.log(req.cookies);
+})
+
+app.get("/user/:id", async (req, res) => {
+  const query = { uid: req.params.id };
+  const user = await userCol.findOne(query);
+  const booked = await bookedCol.findOne(query);
+  res.send({ user, booked });
+});
+
+app.post("/newuser", async (req, res) => {
+  const newBookings = { uid: req.body.uid, wishlist: [] };
+  const bookedData = await bookedCol.insertOne(newBookings);
+  const newUser = {
+    uid: req.body.uid,
+    email: req.body.email,
+    name: req.body.name,
+    photo: req.body.photo,
+    role: req.body.role,
+    bookedId: bookedData.insertedId,
+  };
+  console.log(`creating new user with data`, newUser);
+  const result = await userCol.insertOne(newUser);
+  res.send(result);
 });
